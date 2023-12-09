bc56d5e742d5580a36cadbc1d3bfc1fb853b9dd3
griff182uk <griff182uk@yahoo.co.uk>
2023-12-09 14:30:24 +0000

    Updated terraform template and domain.env file

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Azure Container App Deployment Learning
 
+- [Idea Sourced from Medium](https://medium.com/@abhimanyubajaj98/seamless-azure-container-apps-deployment-with-terraform-c2e56d719465)
+
+Updated my own template terraform in this clone. Updated to latest azurerm provider and allowed terraform.sh to apply if I pass in param.
+
+```bash
+sh ./cde/terraform.sh True
+```
+
+## Notes
+
+- Container reg, container environment and log analytics should be in separate platform hosted. In here just for quick try.
+- First run of terraform seemed to create container app all fine portal but terraform failed and the activity log failed. Terraform state file did not bring in the new resource even though was there, so I had to remove and start again...
+- Issue with deployment of container app due to ingress, see [issue here](https://github.com/hashicorp/terraform-provider-azurerm/issues/20435). HAve tried the config trichling suggested in my terraform. And yes this works.
--- terraform/main.tf
+++ terraform/main.tf
@@ -3,3 +3,53 @@ resource "azurerm_resource_group" "rg" {
   location = var.region
   tags     = local.tags
 }
+
+resource "azurerm_log_analytics_workspace" "analytics_workspace" {
+  name                = local.la_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+  tags                = local.tags
+}
+
+resource "azurerm_container_app_environment" "app_environment" {
+  name                       = local.ace_name
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.analytics_workspace.id
+  tags                       = local.tags
+}
+resource "azurerm_container_app" "app" {
+  name                         = local.container_app_name
+  container_app_environment_id = azurerm_container_app_environment.app_environment.id
+  resource_group_name          = azurerm_resource_group.rg.name
+  tags                         = local.tags
+  revision_mode                = "Multiple"
+  template {
+    container {
+      name   = "examplecontainerapp2"
+      image  = "mcr.microsoft.com/azuredocs/containerapps-helloworld:latest"
+      cpu    = 0.25
+      memory = "0.5Gi"
+    }
+  }
+  ingress {
+    external_enabled = true
+    target_port      = 80
+    traffic_weight {
+      latest_revision = true
+      percentage      = 100
+    }
+  }
+
+}
+
+output "azurerm_container_app_url" {
+  value = azurerm_container_app.app.latest_revision_fqdn
+}
+
+output "azurerm_container_app_revision_name" {
+  value = azurerm_container_app.app.latest_revision_name
+
+}
